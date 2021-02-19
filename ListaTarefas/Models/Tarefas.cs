adf7192a85ee4763cdc5294b1b6c290208262696
ThierrySilva <thierry.silva.tiago@gmail.com>
2021-02-19 01:10:40 -0300

    Primeiro commit

---
+++ ListaTarefas/Models/Tarefas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ListaTarefas.Models
+{
+    public class Tarefas
+    {
+        public int TarefasId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public DateTime Inicio { get; set; }
+
+        public DateTime Fim { get; set; }
+
+        public string Importancia { get; set; }
+
+    }
+}
---
+++ ListaTarefas/Models/TarefasContexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ListaTarefas.Models
+{
+    public class TarefasContexto : DbContext
+    {
+
+        public DbSet<Tarefas> Tarefas { get; set; }
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+        public TarefasContexto(DbContextOptions<TarefasContexto> opcoes) : base(opcoes)
+        {
+
+        }
+      
+    }
+}
